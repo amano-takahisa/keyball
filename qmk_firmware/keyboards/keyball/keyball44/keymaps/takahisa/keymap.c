cce5e3aa999f625075d444fe903cad31333551e3
Taka <amano.takahisa@gmail.com>
2026-03-19 21:57:22 +0100

    tab, ctrl, alt, space, enter, bksp, del

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/takahisa/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/takahisa/keymap.c
@@ -35,10 +35,10 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_right_ball(
-    XXXXXXX  , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , XXXXXXX  ,
-    XXXXXXX  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , XXXXXXX  , XXXXXXX  ,
-    XXXXXXX  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
-              XXXXXXX  , XXXXXXX  , XXXXXXX             , XXXXXXX  , XXXXXXX  ,                  XXXXXXX  , XXXXXXX  ,                                  XXXXXXX
+    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , XXXXXXX  ,
+    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , XXXXXXX  , XXXXXXX  ,
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                  KC_N     , KC_M     , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
+                          KC_LWIN  , KC_LALT  ,     XXXXXXX  , KC_SPC   , KC_DEL   ,     KC_BSPC  , KC_ENT   ,                                     XXXXXXX
   ),
 
   [1] = LAYOUT_right_ball(
